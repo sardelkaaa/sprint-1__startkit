b6a6ac00f1b4c5846098b31a23eae8093b34669e
Sagitova Adelia <adeliya64775@gmail.com>
2024-12-21 16:26:37 +0500

    fix: token

--- src/components/api.js
+++ src/components/api.js
@@ -1,7 +1,7 @@
 const config = {
-    baseUrl: 'https://nomoreparties.co/v1/frontend-st-cohort-201',
+    baseUrl: 'https://nomoreparties.co/v1/apf-cohort-202',
     headers: {
-      authorization: 'b9150b16-1a08-44d7-a100-9603c9e6e81c',
+      authorization: '97040fde-2249-4664-b748-5787a7c167f5',
       'Content-Type': 'application/json'
     }
   }
